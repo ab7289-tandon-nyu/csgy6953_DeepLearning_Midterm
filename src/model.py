9fbb9797dad5182a9f9bc138dac2993fb3882ef6
Alex Biehl <ab7289@nyu.edu>
2022-11-06 09:32:20 -0500

    added dropout config to model

--- src/model.py
+++ src/model.py
@@ -9,7 +9,7 @@ class ResidualBlock(nn.Module):
     Class representing a convolutional residual block 
     '''
 
-    def __init__(self, num_channels: int, use_stem: bool = False, strides: int = 1):
+    def __init__(self, num_channels: int, use_stem: bool = False, strides: int = 1, dropout: Optional[float] = None):
         '''
         Creates a new instance of a Residual Block
         @param: num_channels (int) - the number of output channels for all convolutions in 
@@ -17,12 +17,15 @@ class ResidualBlock(nn.Module):
         @param: use_stem (bool) - whether a 1x1 convolution is needed to downsample the
             residual
         @param: strides (int) - the number of strides to use in the convolutions, defaults to 1
+        @param: dropout (float) - if present, adds a dropout between the hidden layers
         '''
         super().__init__()
         self.num_channels = num_channels
         self.use_stem = use_stem
         self.strides = strides
 
+        self.dropout = nn.Dropout(
+            dropout, inplace=True) if dropout is not None else None
         self.conv1 = nn.LazyConv2d(
             num_channels, kernel_size=3, padding=1, stride=strides)
         self.conv2 = nn.LazyConv2d(
@@ -40,6 +43,8 @@ class ResidualBlock(nn.Module):
     def forward(self, inputs: torch.Tensor) -> torch.Tensor:
         shortcut = inputs
         x = self.relu(self.bn1(self.conv1(inputs)))
+        if self.dropout is not None:
+            x = self.dropout(x)
         x = self.bn2(self.conv2(x))
         if self.use_stem:
             # downsample skip connection
@@ -55,6 +60,7 @@ class StemConfig:
     convenience class to encapsulate configuration options
     for the ResNet stem
     '''
+
     def __init__(self, num_channels, kernel_size, stride, padding):
         self.num_channels = num_channels
         self.kernel_size = kernel_size
@@ -62,13 +68,12 @@ class StemConfig:
         self.padding = padding
 
 
-
 class ResNet(nn.Module):
     '''
     Class representing a full ResNet model
     '''
 
-    def __init__(self, architecture: List[Tuple[int,int]], stem_config: Optional[StemConfig], output_size: int = 10, *args, **kwargs):
+    def __init__(self, architecture: List[Tuple[int, int, float]], stem_config: Optional[StemConfig], output_size: int = 10, *args, **kwargs):
         '''
         returns an instance of a ResNet
         '''
@@ -86,8 +91,8 @@ class ResNet(nn.Module):
 
         self.body = nn.Sequential()
         for idx, layer_def in enumerate(architecture):
-            self.body.add_module(f"block_{idx+2}", self.create_block(*layer_def, first_block=(idx == 0)))
-
+            self.body.add_module(
+                f"block_{idx+2}", self.create_block(*layer_def, first_block=(idx == 0)))
 
     def forward(self, inputs: torch.Tensor) -> torch.Tensor:
         """
@@ -96,7 +101,6 @@ class ResNet(nn.Module):
         x = self.stem(inputs)
         x = self.body(x)
         return self.classifier(x)
-
 
     def create_stem(self, num_channels: int = 64, kernel_size: int = 7, stride: int = 2, padding: int = 3) \
             -> nn.Sequential:
@@ -121,12 +125,12 @@ class ResNet(nn.Module):
             nn.LazyLinear(num_classes)
         )
 
-    def create_block(self, num_residuals: int, num_channels: int, first_block: bool = False) -> nn.Sequential:
+    def create_block(self, num_residuals: int, num_channels: int, dropout: float, first_block: bool = False) -> nn.Sequential:
         layer = []
         for i in range(num_residuals):
             if i == 0 and not first_block:
-                layer.append(ResidualBlock(num_channels, use_stem=True, strides=2))
+                layer.append(ResidualBlock(
+                    num_channels, dropout=dropout, use_stem=True, strides=2))
             else:
-                layer.append(ResidualBlock(num_channels))
+                layer.append(ResidualBlock(num_channels, dropout=dropout))
         return nn.Sequential(*layer)
-        
