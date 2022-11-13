0b55c7eb50549045beb203f60a741d23d273b1c0
os0000os <117290043+os0000os@users.noreply.github.com>
2022-11-13 08:42:18 -0500

    Added to utils.py a function that counts params
    also annotated the initialize_parameter function

--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,9 @@
 import torch
 from torch import nn
 
+# precondition: any lazy convolutional layers 
+# have been converted to normal convolution layers, 
+# for example by passing a dummy input tensor through the model 
 def initialize_parameters(m: nn.Module) -> None:
     if isinstance(m, nn.Conv2d):
         nn.init.kaiming_normal_(m.weight.data, nonlinearity = 'relu')
@@ -17,3 +20,14 @@ def epoch_time(start_time, end_time):
     elapsed_mins = int(elapsed_time / 60)
     elapsed_secs = int(elapsed_time - (elapsed_mins * 60))
     return elapsed_mins, elapsed_secs
+
+
+def count_parameters(model):
+    num_parameters = 0
+    num_parameters_requiring_grad = 0
+    for p in model.parameters():
+        numel = p.numel()
+        num_parameters += numel
+        if p.requires_grad:
+            num_parameters_requiring_grad += numel
+    return num_parameters, num_parameters_requiring_grad
