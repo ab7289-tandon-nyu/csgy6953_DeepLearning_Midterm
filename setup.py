15e6a88d0204d03eb76dfab9b772d493f27eb7d3
alexander-biehl <biehl.al@gmail.com>
2022-11-14 18:27:36 -0500

    added test dependencies to setup.cfg and requirements.txt

--- setup.py
+++ setup.py
@@ -9,10 +9,21 @@ dependencies = [
     "torch-summary",
 ]
 
+test_dependencies = [
+    "pytest",
+    "flake8",
+    "black",
+    "isort",
+    "coverage",
+    "coveralls"
+]
+
 setup(
     name="ResNetSrc",
     version="0.0.1-dev",
     packages=["src"],
     install_requires=dependencies,
     include_package_data=True,
+    tests_require=test_dependencies,
+    test_suite="tests",
 )
