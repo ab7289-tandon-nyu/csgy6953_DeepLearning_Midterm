60aee39b12b2670a3356274d6d5944003cfe3d7d
os0000os <117290043+os0000os@users.noreply.github.com>
2022-11-16 17:04:49 -0500

    created a partial solution that stops working when in_n becomes 1

--- src/model.py
+++ src/model.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 
 from typing import List, Tuple, Optional
 
@@ -9,7 +10,7 @@ class ResidualBlock(nn.Module):
     Class representing a convolutional residual block 
     '''
 
-    def __init__(self, num_channels: int, use_stem: bool = False, strides: int = 1, dropout: Optional[float] = None, kernel_size: int = 2):
+    def __init__(self, num_channels: int, use_stem: bool = False, strides: int = 1, dropout: Optional[float] = None, kernel_size: int = 3):
         '''
         Creates a new instance of a Residual Block
         @param: num_channels (int) - the number of output channels for all convolutions in 
@@ -24,11 +25,25 @@ class ResidualBlock(nn.Module):
         self.use_stem = use_stem
         self.strides = strides
 
+        self.kernel_size = kernel_size
+
         self.dropout = nn.Dropout(dropout) if dropout is not None else None
-        self.conv1 = nn.LazyConv2d(
-            num_channels, kernel_size=kernel_size, padding=1, stride=strides)
-        self.conv2 = nn.LazyConv2d(
-            num_channels, kernel_size=kernel_size, padding=1)
+
+        if self.kernel_size == 3:
+            self.conv1 = nn.LazyConv2d(num_channels, kernel_size=3, padding=1, stride=strides)
+            self.conv2 = nn.LazyConv2d(num_channels, kernel_size=3, padding=1)
+
+        # partial solution 1:
+        if kernel_size == 2:
+            self.conv1 = nn.LazyConv2d(num_channels, kernel_size=2, padding=1, stride=strides)
+            self.conv2 = nn.LazyConv2d(num_channels, kernel_size=2, padding=0)
+            # fetal issue: when input is [256,1,1], MUST pad in order to apply 2x2 kernel
+
+        # # partial solution 2:
+        # if self.kernel_size == 2:
+        #     self.conv1 = nn.LazyConv2d(num_channels, kernel_size=2, padding=1, stride=strides)
+        #     self.conv2 = nn.LazyConv2d(num_channels, kernel_size=2, padding=1)
+
         self.relu = nn.ReLU(inplace=True)
         self.out = nn.ReLU(inplace=True)
         self.bn1 = nn.LazyBatchNorm2d()
@@ -36,21 +51,50 @@ class ResidualBlock(nn.Module):
 
         self.conv_stem = None
         if use_stem:
-            self.conv_stem = nn.LazyConv2d(
-                num_channels, kernel_size=1, stride=strides)
+
+            if kernel_size == 3:
+                self.conv_stem = nn.LazyConv2d(num_channels, kernel_size=1, stride=strides)
+            if kernel_size == 2:
+                self.conv_stem = nn.LazyConv2d(num_channels, kernel_size=1, stride=strides)
 
     def forward(self, inputs: torch.Tensor) -> torch.Tensor:
+        print()
+        print('--------ResidualBlock:--------')
+        print(f'(use_stem={self.use_stem})')
+        print('block input:', inputs.shape)
+
         shortcut = inputs
+
+        print()
+        print('F(x):', self.conv1)
         x = self.relu(self.bn1(self.conv1(inputs)))
+        print('output:', x.shape)
+
         if self.dropout is not None:
             x = self.dropout(x)
+        
+        print()
+        print('F(x):', self.conv2)
         x = self.bn2(self.conv2(x))
+        print('output:', x.shape)
+
         if self.use_stem:
             # downsample skip connection
+            print()
+            print('S(x):', self.conv_stem)
             shortcut = self.conv_stem(shortcut)
+            print('output:', shortcut.shape)
 
+        # partial solution 2:
+        # if self.kernel_size == 2:
+        #     if x.shape[-1] > shortcut.shape[-1]:
+        #         x = F.pad(x, pad = (-1, -1, -1, -1))
+        #         print('negative padding =>', x.shape)
+        #         # fetal error: when F(x)'s output becomes [512,2,2] whiel input was [256,1,1], this would reduce F(x)'s output to [512,0,0]
+ 
         # add in skip connection
         x += shortcut
+
         return self.out(x)
 
 
@@ -124,12 +168,11 @@ class ResNet(nn.Module):
             nn.LazyLinear(num_classes)
         )
 
-    def create_block(self, num_residuals: int, num_channels: int, dropout: float, first_block: bool = False) -> nn.Sequential:
+    def create_block(self, num_residuals: int, num_channels: int, dropout: float, kernel_size: int, first_block: bool = False) -> nn.Sequential:
         layer = []
         for i in range(num_residuals):
             if i == 0 and not first_block:
-                layer.append(ResidualBlock(
-                    num_channels, dropout=dropout, use_stem=True, strides=2))
+                layer.append(ResidualBlock(num_channels, dropout=dropout, kernel_size=kernel_size, use_stem=True, strides=2))
             else:
-                layer.append(ResidualBlock(num_channels, dropout=dropout))
+                layer.append(ResidualBlock(num_channels, dropout=dropout, kernel_size=kernel_size))
         return nn.Sequential(*layer)
